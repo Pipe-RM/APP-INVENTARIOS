a69752b69ba3b2f81c381aa0b8fc224b78b6abba
migue <miguelolvhdz@gmail.com>
2025-06-06 01:03:35 -0600

    06/06/2025 Implementación completa de recycler

--- app/src/main/java/com/example/proyecto11/MCategoriasAlimentos/BebidasCategorias.kt
+++ app/src/main/java/com/example/proyecto11/MCategoriasAlimentos/BebidasCategorias.kt
@@ -1,11 +1,14 @@
 package com.example.proyecto11.MCategoriasAlimentos
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.proyecto11.R
+import com.example.proyecto11.ScrollAlimentos
 
 class BebidasCategorias : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,17 @@ class BebidasCategorias : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun abrirMenuFrios(view: View){
+        val intent = Intent(this, ScrollAlimentos::class.java)
+        intent.putExtra("tv_titulo_menu", "Frios")
+        startActivity(intent)
+    }
+
+    fun abrirMenuCalientes(view: View){
+        val intent = Intent(this, ScrollAlimentos::class.java)
+        intent.putExtra("tv_titulo_menu", "Calientes")
+        startActivity(intent)
     }
 }
